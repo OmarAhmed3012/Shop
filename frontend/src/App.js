e8ad73de6670bd89d318c09220b62adf0e6ef352
OmarAhmed3012 <omaraboshamaa@gmail.com>
2020-11-25 18:40:32 +0200

    Product components stage 1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,19 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import HomeScreen from './screens/HomeScreen'
+
 function App() {
   return (
     <>
-      <h1>Welcome</h1>
+      <Header></Header>
+      <main className="py-3">
+        <Container>
+          <HomeScreen></HomeScreen>
+        </Container>
+      </main>
+      <Footer></Footer>
     </>
   )
 }
