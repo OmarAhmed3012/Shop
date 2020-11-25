cb25d7b946930f21a5180a27eeaee62cb49edad0
OmarAhmed3012 <omaraboshamaa@gmail.com>
2020-11-25 19:38:53 +0200

    Links and LinkContainer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,23 @@
 import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
 import Footer from './components/Footer'
 import Header from './components/Header'
 import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
 
 function App() {
   return (
-    <>
+    <Router>
       <Header></Header>
       <main className="py-3">
         <Container>
-          <HomeScreen></HomeScreen>
+          <Route path="/" component={HomeScreen} exact></Route>
+          <Route path="/product/:id" component={ProductScreen}></Route>
         </Container>
       </main>
       <Footer></Footer>
-    </>
+    </Router>
   )
 }
 
