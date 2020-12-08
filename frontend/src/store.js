020eb2c48312e22b9054f7dbe032fb7829ab516e
OmarAhmed3012 <omaraboshamaa@gmail.com>
2020-12-08 18:20:34 +0200

    Setting Redux and bringing products to HS

---
+++ frontend/src/store.js
@@ -0,0 +1,24 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import {
+  productListReducer,
+  productDetailsReducer,
+} from './reducers/productReducers.js'
+
+const reducer = combineReducers({
+  productList: productListReducer,
+  productDetails: productDetailsReducer,
+})
+
+const initialState = {}
+
+const middlware = [thunk]
+
+const store = createStore(
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middlware))
+)
+
+export default store
