ee180dc40f36218f5ca9aed0cefdcd0094238abe
OmarAhmed3012 <omaraboshamaa@gmail.com>
2020-12-13 01:49:38 +0200

    user Register

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -9,7 +9,7 @@ const authUser = asyncHandler(async (req, res) => {
   const user = await User.findOne({ email })
 
   if (user && (await user.matchPassword(password))) {
-    res.json({
+    res.status(200).json({
       _id: user._id,
       name: user.name,
       email: user.email,
@@ -24,4 +24,51 @@ const authUser = asyncHandler(async (req, res) => {
   res.send({ email, password })
 })
 
-export { authUser }
+//register a new user
+const registerUser = asyncHandler(async (req, res) => {
+  const { email, password, name } = req.body
+
+  const userExists = await User.findOne({ email })
+
+  if (userExists) {
+    res.status(400)
+    throw new Error('User already exists!')
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  })
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    })
+  } else {
+    res.status(400)
+    throw new Error('invalid user data')
+  }
+})
+
+//get user profile
+const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id)
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    })
+  } else {
+    res.status(404)
+    throw new Error('user not found')
+  }
+})
+
+export { authUser, getUserProfile, registerUser }
