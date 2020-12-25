b2df8112e901d8a64a308c4a9a6f10c217b9ee78
OmarAhmed3012 <omaraboshamaa@gmail.com>
2020-12-26 00:02:40 +0200

    PayPal Payments

--- backend/server.js
+++ backend/server.js
@@ -1,41 +1,45 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import connectDB from './config/db.js'
-import colors from 'colors'
+import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from './config/db.js';
+import colors from 'colors';
 
-import productRoutes from './routes/productRoutes.js'
-import userRoutes from './routes/userRoutes.js'
-import orderRoutes from './routes/orderRoutes.js'
-import { errorHandeler, notFound } from './middleware/errorMiddleware.js'
+import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
+import orderRoutes from './routes/orderRoutes.js';
+import { errorHandeler, notFound } from './middleware/errorMiddleware.js';
 
-dotenv.config()
+dotenv.config();
 
-connectDB()
+connectDB();
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('api is running')
-})
+  res.send('api is running');
+});
 
 //routers
-app.use('/api/products', productRoutes)
+app.use('/api/products', productRoutes);
 
-app.use('/api/users', userRoutes)
+app.use('/api/users', userRoutes);
 
-app.use('/api/orders', orderRoutes)
+app.use('/api/orders', orderRoutes);
+
+app.get('/api/config/paypal', (req, res) => {
+  res.send(process.env.PAYPAL_CLIENT_ID);
+});
 
 //error middlewares
-app.use(notFound)
-app.use(errorHandeler)
+app.use(notFound);
+app.use(errorHandeler);
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
 app.listen(
   port,
   console.log(
     `server is running ${process.env.NODE_ENV} on port ${port}`.yellow.bold
   )
-)
+);
