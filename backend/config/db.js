cf3f180381270550d5788c37f07b77bd36458cb7
OmarAhmed3012 <omaraboshamaa@gmail.com>
2021-01-05 16:33:20 +0200

    Admin middleware and get users endpoint

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,18 +1,21 @@
-import mongoose from 'mongoose'
+import mongoose from 'mongoose';
+const url = 'mongodb://localhost:27017/Shop';
+
+//const url = process.env.MONGO_URI
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
+    const conn = await mongoose.connect(url, {
       useUnifiedTopology: true,
       useNewUrlParser: true,
       useCreateIndex: true,
-    })
+    });
 
-    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline)
+    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
   } catch (error) {
-    console.error(`Error: ${error.message}`.red.underline.bold)
-    process.exit(1)
+    console.error(`Error: ${error.message}`.red.underline.bold);
+    process.exit(1);
   }
-}
+};
 
-export default connectDB
+export default connectDB;
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,18 +1,19 @@
-import express from 'express'
-const router = express.Router()
+import express from 'express';
+const router = express.Router();
 import {
   authUser,
   registerUser,
   getUserProfile,
   updateUserProfile,
-} from '../controllers/userController.js'
-import { protect } from '../middleware/authMiddleware.js'
+  getUsers,
+} from '../controllers/userController.js';
+import { protect, admin } from '../middleware/authMiddleware.js';
 
-router.route('/').post(registerUser)
-router.post('/login', authUser)
+router.route('/').post(registerUser).get(protect, admin, getUsers);
+router.post('/login', authUser);
 router
   .route('/profile')
   .get(protect, getUserProfile)
-  .put(protect, updateUserProfile)
+  .put(protect, updateUserProfile);
 
-export default router
+export default router;
