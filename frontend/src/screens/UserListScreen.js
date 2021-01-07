557e68b8aa7d3ed4aaf1a9f7e4c5c8e91ea4c699
OmarAhmed3012 <omaraboshamaa@gmail.com>
2021-01-07 17:00:37 +0200

    admin delete user

--- frontend/src/screens/UserListScreen.js
+++ frontend/src/screens/UserListScreen.js
@@ -5,6 +5,7 @@ import Message from './../components/Message';
 import Loader from './../components/Loader';
 import { LinkContainer } from 'react-router-bootstrap';
 import { listUsers } from '../actions/userActions';
+import { deleteUser } from './../actions/userActions';
 
 const UserListScreen = ({ history }) => {
   const dispatch = useDispatch();
@@ -15,16 +16,21 @@ const UserListScreen = ({ history }) => {
   const userLogin = useSelector((state) => state.userLogin);
   const { userInfo } = userLogin;
 
+  const userDelete = useSelector((state) => state.userDelete);
+  const { success: successDelete } = userDelete;
+
   useEffect(() => {
     if (userInfo && userInfo.isAdmin) {
       dispatch(listUsers());
     } else {
       history.push('/login');
     }
-  }, [dispatch, history]);
+  }, [dispatch, history, successDelete]);
 
   const deleteHandler = (id) => {
-    console.log('delete');
+    if (window.confirm('Are you sure?')) {
+      dispatch(deleteUser(id));
+    }
   };
   return (
     <>
