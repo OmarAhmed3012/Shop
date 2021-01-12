add241eafc3367eeaf4cbb7adb34f516287be208
OmarAhmed3012 <omaraboshamaa@gmail.com>
2021-01-12 18:14:49 +0200

    Pagination front and back, user and admin

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -38,6 +38,12 @@ function App() {
           <Route
             path="/admin/productlist"
             component={ProductListScreen}
+            exact
+          ></Route>
+          <Route
+            path="/admin/productlist/:pageNumber"
+            component={ProductListScreen}
+            exact
           ></Route>
           <Route path="/admin/user/:id/edit" component={UserEditScreen}></Route>
           <Route
@@ -45,7 +51,13 @@ function App() {
             component={ProductEditScreen}
           ></Route>
           <Route path="/cart/:id?" component={CartScreen}></Route>
-          <Route path="/search/:keyword" component={HomeScreen}></Route>
+          <Route path="/search/:keyword" component={HomeScreen} exact></Route>
+          <Route path="/page/:pageNumber" component={HomeScreen} exact></Route>
+          <Route
+            path="/search/:keyword/page/:pageNumber"
+            component={HomeScreen}
+            exact
+          ></Route>
           <Route path="/" component={HomeScreen} exact></Route>
         </Container>
       </main>
